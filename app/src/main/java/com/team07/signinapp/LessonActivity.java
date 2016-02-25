b3c6e53853093cad4f6de4097b7a3f9d56812895
purplegoat53 <J.Rose@dundee.ac.uk>
2016-02-25 16:47:55 +0000

    cleaned up debug code

--- app/src/main/java/com/team07/signinapp/LessonActivity.java
+++ app/src/main/java/com/team07/signinapp/LessonActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -126,7 +125,6 @@ public class LessonActivity extends AppCompatActivity {
                 .setPositiveButton(R.string.attendance_sign_in, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Log.i("TAG", code.getText().toString());
                         if (!Pin.getShared().checkPin(code.getText().toString(), lesson.id)) {
                             // FIX: little messy
                             new AlertDialog.Builder(LessonActivity.this)
