c5b9e28f6bf30d57f061d6b793e9a76fcd0d82de
patarzynak <prus.kasia@gmail.com>
2016-03-18 14:22:00 +0000

    Restructuring the tests.

--- app/src/androidTest/java/com/team07/signinapp/GpsTests.java
+++ app/src/androidTest/java/com/team07/signinapp/GpsTests.java
@@ -1,15 +1,20 @@
 package com.team07.signinapp;
 
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
-public class GpsTests {
+public class GpsTests extends ApplicationTestCase<Application> {
     private static float pyongyangLatitude = 39.0392193f;
     private static float pyongyangLongitude = 125.76252410000006f;
     private static float catIslandLatitude = 24.213309f;
     private static float catIslandLongitude = -75.36453689999996f;
     private static float distanceBetweenPyongyangAndCatIslandMeters = 12650286f;
     private static float distanceToleranceMeters = 15f;
+
+    public GpsTests (){super(Application.class);}
 
     @Test
     // Check whether distance calculation is accurate
--- app/src/androidTest/java/com/team07/signinapp/PinTests.java
+++ app/src/androidTest/java/com/team07/signinapp/PinTests.java
@@ -1,16 +1,22 @@
 package com.team07.signinapp;
 
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
-public class PinTests {
+public class PinTests extends ApplicationTestCase<Application> {
     private static int pinMinimum = 1000;
     private static int pinMaximum = 9999;
 
+    public PinTests(){super(Application.class);}
+
     @Test
     // Ensure that pin generator is generating pin numbers between 1000 and 9999 inclusive
-    public void pinGeneratorWithinBounds() {
+    public void testPinGeneratorWithinBounds() {
         int pin = Pin.createPinValue();
         assertTrue(pin >= pinMinimum && pin <= pinMaximum);
     }
+
 }
